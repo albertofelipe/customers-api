c15547a33723bccca56f20caa35602daf90abf62
Alberto Felipe <felipe0032sena@gmail.com>
2025-04-01 12:15:19 -0300

    Filtering data: CustomersFilter and InvoicesFilter

--- customers-api/app/Http/Controllers/Api/V1/InvoiceController.php
+++ customers-api/app/Http/Controllers/Api/V1/InvoiceController.php
@@ -8,15 +8,25 @@ use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Resources\V1\InvoiceCollection;
+use App\Filters\V1\InvoicesFilter;
+use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new InvoiceCollection(Invoice::paginate());
+        $filter = new InvoicesFilter();
+        $queryItems = $filter->transform($request);
+
+        if (count($queryItems) == 0) {
+            return new InvoiceCollection(Invoice::paginate());
+        } else {
+            $invoices = Invoice::where($queryItems)->paginate();
+            return new InvoiceCollection($invoices->appends($request->query()));
+        }
     }
 
     /**
