8cd65ac9da91707b876b0320e1e140070b8f6641
albertofelipe <felipe0032sena@gmail.com>
2025-04-06 14:34:08 -0300

    Auth - Configura middleware de autenticação com Sanctum

--- customers-api/routes/api.php
+++ customers-api/routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\V1\CustomerController;
 use App\Http\Controllers\Api\V1\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
     
--- customers-api/routes/web.php
+++ customers-api/routes/web.php
@@ -1,7 +1,41 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup', function () {
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+
+    if (!Auth::attempt($credentials)) {
+        $user = new User;
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+        $user->save();
+
+        Auth::attempt($credentials);
+    }
+
+    $user = Auth::user();
+
+    // Revoga tokens antigos antes de criar novos (opcional)
+    $user->tokens()->delete();
+
+    $adminToken = $user->createToken('adminToken', ['create', 'update', 'delete']);
+    $updateToken = $user->createToken('updateToken', ['create', 'update']);
+    $basicToken = $user->createToken('basicToken');
+
+    return [
+        'admin' => $adminToken->plainTextToken,
+        'update' => $updateToken->plainTextToken,
+        'basic' => $basicToken->plainTextToken,
+    ];
 });
